987110e3b665e55696b025ee47d2bc9cffe68516
RJ <randy.j416@yahoo.com>
2022-12-15 14:12:55 -0500

    formatting

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
-sayHello("Caliban")
-; sayHello("Miranda");
+sayHello("Caliban");
+sayHello("Miranda");
 sayHello("Ferdinand");
